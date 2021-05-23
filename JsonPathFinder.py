a15f95621cacc97960fceca61b09c3a02df3f0c7
qiankunxienb <qiankun.xie@newsbreak.com>
2021-05-23 11:46:53 +0800

    now you can search by value

--- JsonPathFinder.py
+++ JsonPathFinder.py
@@ -3,8 +3,9 @@ from typing import List
 
 
 class JsonPathFinder:
-    def __init__(self, json_str):
+    def __init__(self, json_str, mode='key'):
         self.data = json.loads(json_str)
+        self.mode = mode
 
     def iter_node(self, rows, road_step, target):
         if isinstance(rows, dict):
@@ -16,27 +17,44 @@ class JsonPathFinder:
         for key, value in key_value_iter:
             current_path = road_step.copy()
             current_path.append(key)
-            if key == target:
+            if self.mode == 'key':
+                check = key
+            else:
+                check = value
+            if check == target:
                 yield current_path
             if isinstance(value, (dict, list)):
                 yield from self.iter_node(value, current_path, target)
 
-    def find_one(self, key: str) -> list:
-        path_iter = self.iter_node(self.data, [], key)
+    def find_one(self, target: str) -> list:
+        path_iter = self.iter_node(self.data, [], target)
         for path in path_iter:
             return path
         return []
 
-    def find_all(self, key) -> List[list]:
-        path_iter = self.iter_node(self.data, [], key)
+    def find_all(self, target) -> List[list]:
+        path_iter = self.iter_node(self.data, [], target)
         return list(path_iter)
 
 
 if __name__ == '__main__':
     with open('sample.json', ) as f:
         json_data = f.read()
+
+    print('开始测试按 Key 搜索...')
     finder = JsonPathFinder(json_data)
     path_list = finder.find_all('full_text')
     data = finder.data
     for path in path_list:
         print(path)
+
+    print('开始测试按 Value 搜索：...')
+
+    # value_finder = JsonPathFinder(json_data, mode='value')
+    # path_lits = value_finder.find_all(103)
+    # for path in path_lits:
+    #     print('path: ', path)
+    #     value = json.loads(json_data)
+    #     for step in path:
+    #         value = value[step]
+    #     assert value == 103
